bffa113473847b88678608f74c06eb3987168342
kwiats <pawuskwiatkowski@gmail.com>
2022-11-13 20:44:23 +0100

    Change some urls

--- cards/urls.py
+++ cards/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("", views.home, name="home"),
     path("add_word/", views.add_word, name="add_word"),
     path("add_category/", views.add_category, name="add_category"),
-    path("", views.flashcards, name="flashcards"),
+    path("home/", views.flashcards, name="flashcards"),
 ]
