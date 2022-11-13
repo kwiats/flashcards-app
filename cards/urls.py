e3e996e4cc91767be813bb07406c4406fb8d94d1
kwiats <pawuskwiatkowski@gmail.com>
2022-11-13 15:02:39 +0100

    Change homepage

--- cards/urls.py
+++ cards/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("add_word/", views.add_word, name="add_word"),
     path("add_category/", views.add_category, name="add_category"),
-    path("test/", views.flashcards, name="flashcards"),
+    path("", views.flashcards, name="flashcards"),
 ]
