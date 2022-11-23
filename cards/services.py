cf5d3a75124161dad1ba8a7e7f280194cd47a08c
kwiats <pawuskwiatkowski@gmail.com>
2022-11-23 17:51:22 +0100

    Add function to chek translation

--- cards/services.py
+++ cards/services.py
@@ -1,34 +1,9 @@
-from random import choice
-from typing import List
+from .models import Word
 
 
-class Learning:
-    def __init__(self, quantity: int, all_ids: List):
-        self.case_one, self.case_two, self.case_three = [], [], []
-        self.temp_lst = []
-        self.all_ids = all_ids
-        self.quantity = quantity
-
-    def gen_list_of_words(self) -> List:
-        while len(self.temp_lst) < self.quantity:
-            id = choice(self.all_ids)
-            self.temp_lst.append(id)
-            self.temp_lst = list(set(self.temp_lst))
-        return self.temp_lst
-
-    def get_random_id(self):
-        return choice(self.temp_lst)
-
-    def split_into_groups(self, type_group: List, word_id: int) -> int:
-        if type_group == "-1":
-            self.case_one.append(word_id)
-            return -1
-        elif type_group == "1":
-            self.case_three.append(word_id)
-            return 1
-        else:
-            self.case_two.append(word_id)
-            return 0
-
-    def check_word_in_list(self, word_id: int, lst: List) -> bool:
-        return word_id in lst
+def check_translated_word(pk, user_answer):
+    word = Word.objects.get(pk=pk)
+    translated_word = word.translated_word
+    if translated_word == user_answer:
+        return f"{translated_word} -> {user_answer} OK"
+    return f"{translated_word} -> {user_answer} ERROR"
