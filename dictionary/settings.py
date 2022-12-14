219151e0391203c8ed2bed69de88e49eeaf77629
kwiats <pawuskwiatkowski@gmail.com>
2022-12-14 21:18:33 +0100

    Add env for secret key

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_^wy1j*eb0a$ssa3gy*0e-9w+j3&pgf2g$8livs2y7yl9!!nd="
+SECRET_KEY = env["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
