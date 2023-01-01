82082351cb63998f85b159edcc74835d73e6bbb8
kwiats <pawuskwiatkowski@gmail.com>
2023-01-01 14:48:54 +0100

    Added doc string

--- translations/apps.py
+++ translations/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class TranslationsConfig(AppConfig):
+    """Configuration class for the 'translations' app"""
+
     name = "translations"
     verbose_name = "Translations for word"
--- users/apps.py
+++ users/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """Configuration class for 'users' app."""
+
     name = "users"
     verbose_name = "Users"
