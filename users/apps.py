8186146cba4c12880cb1b67f2958965b795f6760
kwiats <pawuskwiatkowski@gmail.com>
2023-01-01 19:27:34 +0100

    Fixed signals for new users

--- users/apps.py
+++ users/apps.py
@@ -4,5 +4,9 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     """Configuration class for 'users' app."""
 
+    default_auto_field = "django.db.models.BigAutoField"
     name = "users"
     verbose_name = "Users"
+
+    def ready(self) -> None:
+        from . import signals
