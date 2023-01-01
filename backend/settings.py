724548346c852dc66ff052b6c380b5584af47782
kwiats <pawuskwiatkowski@gmail.com>
2023-01-01 14:23:42 +0100

    fixed settings

--- backend/settings.py
+++ backend/settings.py
@@ -123,23 +123,30 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
+UserAttributeSimilarityValidator = (
+    "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+)
+MinimumLengthValidator = (
+    "django.contrib.auth.password_validation.MinimumLengthValidator"
+)
+CommonPasswordValidator = (
+    "django.contrib.auth.password_validation.CommonPasswordValidator"
+)
+NumericPasswordValidator = (
+    "django.contrib.auth.password_validation.NumericPasswordValidator"
+)
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation."
-        / "UserAttributeSimilarityValidator",
+        "NAME": UserAttributeSimilarityValidator,
     },
     {
-        "NAME": "django.contrib.auth.password_validation." 
-        / "MinimumLengthValidator",
+        "NAME": MinimumLengthValidator,
     },
     {
-        "NAME": "django.contrib.auth.password_validation." 
-        / "CommonPasswordValidator",
+        "NAME": CommonPasswordValidator,
     },
     {
-        "NAME": "django.contrib.auth.password_validation." 
-        / "NumericPasswordValidator",
+        "NAME": NumericPasswordValidator,
     },
 ]
 
