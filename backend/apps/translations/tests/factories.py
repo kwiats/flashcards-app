ec4aabac5fff322d8a57afb4e00deda2cf8fbf53
kwiats <pawuskwiatkowski@gmail.com>
2023-01-09 20:02:41 +0100

    Deleted unnecessary lines in factories

--- backend/apps/translations/tests/factories.py
+++ backend/apps/translations/tests/factories.py
@@ -2,28 +2,9 @@ import factory
 
 from faker import Faker
 from apps.translations.models import Word, Category, Translation
-from apps.users.models import User
+from apps.users.tests.factories import ProfileFactory
 
 fake = Faker("pl_PL")
-
-
-class ProfileFactory(factory.django.DjangoModelFactory):
-    username = factory.Faker.user_name()
-    email = factory.Faker.email()
-    password = factory.Faker.password()
-    profile_picture = factory.Faker.file_path(depth=3, category="photo")
-    current_score = factory.Faker.random_int(
-        min=0,
-        max=9999,
-    )
-    spend_score = factory.Faker.random_int(
-        min=0,
-        max=9999,
-    )
-    total_score = current_score + spend_score
-
-    class Meta:
-        model = User
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
--- backend/apps/users/tests/factories.py
+++ backend/apps/users/tests/factories.py
@@ -1,7 +1,6 @@
 import factory
 
 from faker import Faker
-from apps.translations.models import Word, Category, Translation
 from apps.users.models import User
 
 fake = Faker("pl_PL")
@@ -26,45 +25,3 @@ class ProfileFactory(factory.django.DjangoModelFactory):
         model = User
 
 
-class CategoryFactory(factory.django.DjangoModelFactory):
-    user = factory.SubFactory(ProfileFactory)
-    category = factory.Faker.word()
-    words = factory.SubFactory("tests.factories.WordFactory", parent=None)
-    price = factory.Faker.random_int(
-        min=0,
-        max=9999,
-    )
-    isDefault = False
-    isAllow = True
-
-    class Meta:
-        model = Category
-
-
-class WordFactory(factory.django.DjangoModelFactory):
-    user = factory.SubFactory(ProfileFactory)
-    word = factory.Faker.word()
-    category_word = factory.SubFactory(CategoryFactory)
-    status = factory.Faker.random_element(
-        elements=(
-            "PENDING",
-            "APPROVED",
-            "REJECTED",
-        )
-    )
-    updated = factory.Faker.date_time()
-    created = factory.Faker.date_time()
-
-    class Meta:
-        model = Word
-
-
-class TranslationFactory(factory.django.DjangoModelFactory):
-    word = factory.SubFactory(WordFactory)
-    translation = factory.Faker.text(max_nb_chars=255)
-    pronunciation = factory.Faker.word()
-    updated = factory.Faker.date_time()
-    created = factory.Faker.date_time()
-
-    class Meta:
-        model = Translation
