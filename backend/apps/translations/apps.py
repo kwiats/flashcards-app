48eeeb3d99e27ff2a183536d0fd2ea009c6f9679
kwiats <pawuskwiatkowski@gmail.com>
2023-01-09 19:02:34 +0100

    Create runner for pytest-django

--- backend/apps/translations/apps.py
+++ backend/apps/translations/apps.py
@@ -3,8 +3,5 @@ from django.apps import AppConfig
 
 class TranslationsConfig(AppConfig):
     """Configuration class for the 'translations' app"""
-
-    default_auto_field = "django.db.models.BigAutoField"
-
     name = "apps.translations"
     verbose_name = "Translations for word"
