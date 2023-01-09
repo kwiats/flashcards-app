9c51e3ccb5ffcacdf337f37844198d069e9dbf11
Kwiatuh <46192203+kwiats@users.noreply.github.com>
2023-01-09 19:54:22 +0100

    43 change structure move serializers and views to single apps (#44)
    * Create endpoint with urls from users and translations app
    
    * Change structure for projects

---
+++ backend/apps/users/urls.py
@@ -0,0 +1,39 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+from . import views
+
+
+urlpatterns = [
+    path(
+        "auth-token/",
+        obtain_auth_token,
+        name="obtain-auth-token",
+    ),
+    path(
+        "user/",
+        views.UserListView.as_view(),
+        name="all-users",
+    ),
+    path(
+        "user/<int:pk>/",
+        views.UserDetailView.as_view(),
+        name="specific-user",
+    ),
+    path(
+        "user/<int:pk>/change-password/",
+        views.ChangePasswordView.as_view(),
+        name="change-password",
+    ),
+    path(
+        "ranking/",
+        views.RankingListView.as_view(),
+        name="score-ranking",
+    ),
+    path(
+        "user/<int:pk>/score/",
+        views.ScoreUserView.as_view(),
+        name="total-user-score",
+    ),
+]
