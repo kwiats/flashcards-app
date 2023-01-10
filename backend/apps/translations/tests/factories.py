c9e6815db829e34fc32f293527cf5359458605a4
kwiats <pawuskwiatkowski@gmail.com>
2023-01-10 19:39:18 +0100

    Create first test for word model

--- backend/apps/translations/tests/factories.py
+++ backend/apps/translations/tests/factories.py
@@ -9,9 +9,10 @@ fake = Faker("pl_PL")
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     user = factory.SubFactory(ProfileFactory)
-    category = factory.Faker.word()
+    category = factory.Faker("word")
     words = factory.SubFactory("tests.factories.WordFactory", parent=None)
-    price = factory.Faker.random_int(
+    price = factory.Faker(
+        "random_int",
         min=0,
         max=9999,
     )
@@ -24,17 +25,18 @@ class CategoryFactory(factory.django.DjangoModelFactory):
 
 class WordFactory(factory.django.DjangoModelFactory):
     user = factory.SubFactory(ProfileFactory)
-    word = factory.Faker.word()
+    word = factory.Faker("word")
     category_word = factory.SubFactory(CategoryFactory)
-    status = factory.Faker.random_element(
+    status = factory.Faker(
+        "random_element",
         elements=(
             "PENDING",
             "APPROVED",
             "REJECTED",
-        )
+        ),
     )
-    updated = factory.Faker.date_time()
-    created = factory.Faker.date_time()
+    updated = factory.Faker("date_time")
+    created = factory.Faker("date_time")
 
     class Meta:
         model = Word
@@ -42,10 +44,10 @@ class WordFactory(factory.django.DjangoModelFactory):
 
 class TranslationFactory(factory.django.DjangoModelFactory):
     word = factory.SubFactory(WordFactory)
-    translation = factory.Faker.text(max_nb_chars=255)
-    pronunciation = factory.Faker.word()
-    updated = factory.Faker.date_time()
-    created = factory.Faker.date_time()
+    translation = factory.Faker("text", max_nb_chars=255)
+    pronunciation = factory.Faker("word")
+    updated = factory.Faker("date_time")
+    created = factory.Faker("date_time")
 
     class Meta:
         model = Translation
