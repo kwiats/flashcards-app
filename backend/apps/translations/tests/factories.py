012e1c6cfd91fad1b063ec362af353f66957300e
kwiats <pawuskwiatkowski@gmail.com>
2023-01-15 12:16:53 +0100

    Fixed error with many-to-many relation

--- backend/apps/translations/tests/factories.py
+++ backend/apps/translations/tests/factories.py
@@ -1,20 +1,24 @@
 from apps.users.tests.factories import ProfileFactory
-from factory import Faker, SubFactory
+from factory import LazyFunction, RelatedFactory, SubFactory, post_generation
 from factory.django import DjangoModelFactory
+from faker import Faker
+from faker.providers import internet, misc
+
+faker = Faker()
+faker.add_provider(internet)
+faker.add_provider(misc)
 
 
 class WordFactory(DjangoModelFactory):
     user = SubFactory(ProfileFactory)
-    word = Faker("word")
-    category_word = SubFactory(
-        "apps.translations.tests.factories.CategoryFactory"
-    )  # noqa
-    status = Faker(
-        "random_element",
-        elements=("PENDING", "APPROVED", "REJECTED"),
+    word = LazyFunction(lambda: faker.word())
+    status = LazyFunction(
+        lambda: faker.random_element(
+            elements=("PENDING", "APPROVED", "REJECTED"),
+        )
     )
-    updated = Faker("date_time")
-    created = Faker("date_time")
+    updated = LazyFunction(lambda: faker.date_time())
+    created = LazyFunction(lambda: faker.date_time())
 
     class Meta:
         model = "translations.Word"
@@ -22,20 +26,37 @@ class WordFactory(DjangoModelFactory):
 
 class TranslationFactory(DjangoModelFactory):
     word = SubFactory(WordFactory)
-    translation = Faker("word")
-    pronunciation = Faker("word")
-    updated = Faker("date_time")
-    created = Faker("date_time")
+    translation = LazyFunction(lambda: faker.word())
+    pronunciation = LazyFunction(lambda: faker.word())
+    updated = LazyFunction(lambda: faker.date_time())
+    created = LazyFunction(lambda: faker.date_time())
 
     class Meta:
         model = "translations.Translation"
 
 
 class CategoryFactory(DjangoModelFactory):
-    users = None
-    category = Faker("word")
-    price = Faker("random_int", min=0, max=1000)
-    isDefault = Faker("boolean")
+    category = LazyFunction(lambda: faker.word())
+    price = LazyFunction(lambda: faker.random_int(min=0, max=1000))
+    isDefault = LazyFunction(lambda: faker.boolean())
+
+    @post_generation
+    def words(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for word in extracted:
+                self.words.add(word)
+
+    @post_generation
+    def users(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for user in extracted:
+                self.users.add(user)
 
     class Meta:
         model = "translations.Category"
