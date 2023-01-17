98338ff80117f3ac7cda6cf72ff234508f9a76e7
kwiats <pawuskwiatkowski@gmail.com>
2023-01-17 18:08:38 +0100

    Create manager file for translations and change names

---
+++ backend/apps/users/managers.py
@@ -0,0 +1,22 @@
+# wysweitla wszystkie elementy posrtowane wedlug
+# 1. spend_score
+# 2. total_score
+# 3. current_score
+from django.db import models
+
+
+class ProfileManager(models.Manager):
+    def filter_by_spend_score(self, ascending=False):
+        if ascending:
+            return self.all().order_by("spend_score")
+        return self.all().order_by("-spend_score")
+
+    def filter_by_current_score(self, ascending=False):
+        if ascending:
+            return self.all().order_by("current_score")
+        return self.all().order_by("-current_score")
+
+    def filter_by_total_score(self, ascending=False):
+        if ascending:
+            return self.all().order_by("total_score")
+        return self.all().order_by("-total_score")
