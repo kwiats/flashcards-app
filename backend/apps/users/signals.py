75c79047cf715056b8035653173c6bcf56eb50cf
kwiats <pawuskwiatkowski@gmail.com>
2023-01-22 21:29:55 +0100

    deleted print in signals

--- backend/apps/users/signals.py
+++ backend/apps/users/signals.py
@@ -9,7 +9,6 @@ from .models import Profile
 def create_user_default_list_of_words(sender, instance, created, **kwargs):
     if created:
         default_category = Category.objects.filter(isDefault=True).first()
-        print(default_category)
         if default_category:
             default_category.users.add(instance)
         else:
